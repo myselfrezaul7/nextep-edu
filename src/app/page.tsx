379e71f5140ac724141dbcf8139566bb99d5d9fb
shawono7 <myselfrezaul@outlook.com>
2026-02-06 01:51:08 +0100

    feat: remove next-intl i18n and downgrade to Next.js 15 for stability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import { redirect } from 'next/navigation';
+import { HeroSection } from "@/components/home/HeroSection";
+import { ServicesSection } from "@/components/home/ServicesSection";
+import { AboutSection } from "@/components/home/AboutSection";
+import { TestimonialCarousel } from "@/components/home/TestimonialCarousel";
 
-export default function RootPage() {
-    redirect('/en');
+export default function Home() {
+    return (
+        <div className="flex flex-col">
+            <HeroSection />
+            <ServicesSection />
+            <AboutSection />
+            <TestimonialCarousel />
+        </div>
+    );
 }
